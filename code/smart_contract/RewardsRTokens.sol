b03ee6fbe4c6dce8cdb7d3cff6bc7cbdf3bcefe0
davidlp94 <96163075+davidlp94@users.noreply.github.com>
2022-06-03 17:41:38 -0400

    Updating smart contract doce and added total amount to transactor ipynb file.

--- code/smart_contract/RewardsRTokens.sol
+++ code/smart_contract/RewardsRTokens.sol
@@ -18,4 +18,43 @@ contract RewardsRTokens is ERC20, ERC20Detailed, ERC20Mintable {
             // constructor can stay empty
         }
 
+    address payable accountOne;
+    address payable accountTwo;
+    address payable accountThree;
+    address payable accountFour;
+    address payable accountFive;
+    address payable accountSix;
+    address payable accountSeven;
+    address payable accountEight;
+    address payable accountNine;
+    address payable accountTen;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
+
+    function redeem(uint amount, address payable recipient) public{
+        require(amount <= contractBalance, "You don't have enough funds!");
+
+        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
+        recipient.transfer(amount);
+
+        // Set  `lastWithdrawAmount` equal to `amount`
+        lastWithdrawAmount = amount;
+
+        // Call the `contractBalance` variable and set it equal to the balance of the contract by 
+        // using `address(this).balance` to reflect the new balance of the contract.
+        contractBalance = address(this).balance;
+
+
+    }
+
+    // Define a `public payable` function named `deposit`.
+    function deposit() public payable {
+        contractBalance = address(this).balance;
+
+    }
+
+    // Fallback function
+    function() external payable{}
+
 }
